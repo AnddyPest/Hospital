1c89bbd690b73b21db271ef0af38bb1eb6b1dcbb
AnddyPest <mauripestchanker@gmail.com>
2025-03-27 13:50:46 -0300

    Initial commit

---
+++ src/app/index.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const app = express();
+const router = require("./router/router");
+const morgan = require("morgan");
+const publicPath = __dirname.replace("app", "public");
+
+app.set("port", process.env.PORT || 4040);
+app.set("views", `${publicPath}/templates`);
+app.set("view engine", "pug");
+
+app.use(morgan("dev"));
+app.use("/", router);
+
+module.exports = app;
