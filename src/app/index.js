fede190949783feb046af5db84effd55a0741020
AnddyPest <mauripestchanker@gmail.com>
2025-03-29 18:22:31 -0300

    conection a database
    Habria q ver si hay metodos q permiten crear las tablas si no existen, lo mismo si no existiera la base de datos

--- src/app/index.js
+++ src/app/index.js
@@ -1,8 +1,23 @@
 const express = require("express");
+const mysql = require("mysql");
 const router = require("./router/router");
 const app = express();
 const morgan = require("morgan");
 const publicPath = __dirname.replace("app", "public");
+const conexion = mysql.createConnection({
+  host: "localhost",
+  database: "hospital_db",
+  user: "root",
+  password: "",
+});
+
+conexion.connect((err) => {
+  if (err) {
+    throw err;
+  } else {
+    console.log("CONECTADO A LA BASE DE DATOS");
+  }
+});
 
 app.set("port", process.env.PORT || 3030);
 app.set("views", `${publicPath}/templates`);
