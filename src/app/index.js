4919ba892a929403bcf068e684321c386a1447a6
AnddyPest <mauripestchanker@gmail.com>
2025-03-30 12:49:39 -0300

    conseguido carrousel de imagenes

--- src/app/index.js
+++ src/app/index.js
@@ -6,7 +6,7 @@ const morgan = require("morgan");
 const publicPath = __dirname.replace("app", "public");
 
 app.set("port", process.env.PORT || 3030);
-app.set("views", `${publicPath}/templates`);
+app.set("views", `${publicPath}/views`);
 app.set("view engine", "pug");
 
 app.use(express.static(publicPath));
